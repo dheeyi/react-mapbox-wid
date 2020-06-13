5fcde3f93e7ac0a4424f32a94c91f715cd52fd1f
dheeyi <dheeyi@gmail.com>
2020-06-13 17:39:15 -0400

    Updates

---
+++ precache-manifest.cd33e89953e87bd484b801f96b06161b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbd1609a06d5934efc147223cc8da45",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ae1104f79c02347fd2d5",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "72d58dfe1bf5e5c57e35",
+    "url": "./static/js/2.89cca0f4.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "./static/js/2.89cca0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1104f79c02347fd2d5",
+    "url": "./static/js/main.895781bc.chunk.js"
+  },
+  {
+    "revision": "bad1a5d603fe028528de",
+    "url": "./static/js/runtime-main.df3894bf.js"
+  }
+]);
